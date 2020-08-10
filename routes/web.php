201f46adab4244ac9bfae0ee8417a1f2b9b34781
Mac <mac@Macs-MacBook-Pro.local>
2020-08-10 14:35:09 +0700

    coba vendor

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,6 @@ Route::get('/', function () {
     return view('index');
 });
 
-<<<<<<< HEAD
 Route::get('/proyek','ProyekController@index');
 
 Route::get('/proyek/create','ProyekController@create');
-=======
-Route::get('/proyek/create', 'ItemController@create'); // menampilkan halaman form
-Route::post('/proyek', 'ItemController@store'); // menyimpan data
-Route::get('/proyek/{id}/daftarkan-staff', 'ItemController@create_staff'); // menampilkan halaman form
-Route::post('/proyek/{id}/daftarkan-staff', 'ItemController@store_staff'); // menyimpan data
-Route::get('/proyek', 'ItemController@index'); // menampilkan semua
-Route::get('/proyek/{id}/edit', 'ItemController@edit'); // menampilkan form untuk edit item
-Route::put('/proyek/{id}', 'ItemController@update'); // menyimpan perubahan dari form edit
-Route::delete('/proyek/{id}', 'ItemController@destroy'); // menghapus data dengan id
->>>>>>> 01d1b94bfba476af1255874a7c0442223a3eab5f
